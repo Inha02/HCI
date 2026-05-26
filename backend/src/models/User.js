128b9416eb3212dc7ae15bd1aff96f961f846d91
최인하 <bella020207@naver.com>
2026-05-26 18:17:10 +0900

    Feat: 유저 모델 수정

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,10 +1,39 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  email: String,
-  password: String,
+  // 일반 로그인용
+  email: {
+    type: String,
+    unique: true,
+    sparse: true,
+  },
 
-  name: String,
+  password: {
+    type: String,
+  },
+
+  // 서비스에서 사용할 이름
+  name: {
+    type: String,
+  },
+
+  // 카카오 로그인용
+  socialType: {
+    type: String,
+    enum: ['kakao', 'local'],
+    default: 'local',
+  },
+
+  socialId: {
+    type: String,
+    unique: true,
+    sparse: true,
+  },
+
+  profileImage: {
+    type: String,
+    default: '',
+  },
 
   settings: {
     blinkThreshold: { type: Number, default: 0.2 },
